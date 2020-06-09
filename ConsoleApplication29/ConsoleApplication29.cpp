41bb3e07f1437e413f00724b99a0b610f88bac5c
mvatanasov18 <mvatanasov18@codingburgas.bg>
2020-06-09 22:02:40 +0300

    make easyOption (in progress)

--- ConsoleApplication29/ConsoleApplication29.cpp
+++ ConsoleApplication29/ConsoleApplication29.cpp
@@ -1,9 +1,34 @@
-﻿// ConsoleApplication29.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+#include <fstream>
+#include <time.h>
+#include <vector>
+
 using namespace std;
+
+int giveRandomNum() {
+    int num = rand() % 5;
+    return num;
+}
+
+void easyOption(vector<string>& vect) {
+    vect = { "signal","apple","dog","cat","computer" };
+    cout << vect[giveRandomNum()];
+
+}
+
+void mediumOption() {
+
+}
+
+void advancedOption() {
+
+}
+
+void editMode() {
+
+}
+
 void showInitialGreeting()
 {
     cout << "\n                                     Interoperable Console                       \n";
@@ -12,55 +37,52 @@ void showInitialGreeting()
     cout << "  Please refer to the Documentation for a more detailed explanation about using the program.\n";
     cout << "  Our Team takes no responsibility for misuse of the program.\n\n";
 }
+
 void showGoodbyeMessage()
 {
     cout << "\n  Thank you for using our game!\n\n";
     cout << "  Copyright (c) 2020 team\n";;
 }
+
 bool doShowMenu()
 {
+    vector<string> vect;
     int userInput;
 
     cout << "\n------- Main Menu -------\n";
     cout << "\n1. Easy" << endl;
     cout << "2. Medium" << endl;
-    cout << "3. Hard" << endl;
-    cout << "4. Add word or a definition for a word" << endl;
+    cout << "3. Advanced" << endl;
+    cout << "4. Edit" << endl;
     cout << "5. Quit" << endl;
     cout << "Enter option from the menu by typing a number: ";
     cin >> userInput;
 
     switch (userInput)
     {
-    case 1:
+    case 1:easyOption(vect);
+
         break;
-    case 2:
+    case 2:mediumOption();
         break;
-    case 3:
+    case 3:advancedOption();
         break;
-    case 4:
+    case 4:editMode();
         break;
-    case 5:
-        return 0; showGoodbyeMessage();
+    case 5: {
+        showGoodbyeMessage();
+        return 0;
+        }
         break;
    
     default: cout << "\nPlease enter a valid option!\n"; break;
     }
     return true;
 }
+
 int main()
 {
+    srand(time(NULL));
     showInitialGreeting();
     while (doShowMenu());
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
