b42f506daf86a0503670337f9e16196b9d9795d2
VTStanchev18 <VTStanchev18@codingburgas.bg>
2020-06-09 21:37:49 +0300

    Create showInitialGreeting(), showGoodbyeMessage(), doShowMenu()

--- ConsoleApplication29/ConsoleApplication29.cpp
+++ ConsoleApplication29/ConsoleApplication29.cpp
@@ -4,9 +4,54 @@
 #include <iostream>
 #include <string>
 using namespace std;
+void showInitialGreeting()
+{
+    cout << "\n                                     Interoperable Console                       \n";
+    cout << "  Welcome to our game.\n";
+    cout << "  Thank you for choosing our software!\n";
+    cout << "  Please refer to the Documentation for a more detailed explanation about using the program.\n";
+    cout << "  Our Team takes no responsibility for misuse of the program.\n\n";
+}
+void showGoodbyeMessage()
+{
+    cout << "\n  Thank you for using our game!\n\n";
+    cout << "  Copyright (c) 2020 team\n";;
+}
+bool doShowMenu()
+{
+    int userInput;
+
+    cout << "\n------- Main Menu -------\n";
+    cout << "\n1. Easy" << endl;
+    cout << "2. Medium" << endl;
+    cout << "3. Hard" << endl;
+    cout << "4. Add word or a definition for a word" << endl;
+    cout << "5. Quit" << endl;
+    cout << "Enter option from the menu by typing a number: ";
+    cin >> userInput;
+
+    switch (userInput)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        return 0; showGoodbyeMessage();
+        break;
+   
+    default: cout << "\nPlease enter a valid option!\n"; break;
+    }
+    return true;
+}
 int main()
 {
-    
+    showInitialGreeting();
+    while (doShowMenu());
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
